41d9fbe84fd53ed10010b4c77e3d431298a5215e
poolcue-droid <gaurigthampi@gmail.com>
2026-02-20 09:39:36 +0530

    added synchronization code

--- src/SynchronizationDemo.java
+++ src/SynchronizationDemo.java
@@ -1,22 +1,21 @@
 class Table {
     synchronized void printTable(int n) {
         for (int i = 1; i <= 5; i++) {
-            System.out.print((n * i) + " ");
+            System.out.print(n * i);
+            if (i < 5) System.out.print(" "); 
             try {
-                Thread.sleep(400); 
+                Thread.sleep(400);
             } catch (InterruptedException e) {
                 System.out.println(e);
             }
         }
-        System.out.println(); 
+        System.out.println();
     }
 }
 
 class MyThread1 extends Thread {
     Table t;
-    MyThread1(Table t) {
-        this.t = t;
-    }
+    MyThread1(Table t) { this.t = t; }
     public void run() {
         t.printTable(5);
     }
@@ -24,9 +23,7 @@ class MyThread1 extends Thread {
 
 class MyThread2 extends Thread {
     Table t;
-    MyThread2(Table t) {
-        this.t = t;
-    }
+    MyThread2(Table t) { this.t = t; }
     public void run() {
         t.printTable(100);
     }
@@ -35,11 +32,7 @@ class MyThread2 extends Thread {
 public class SynchronizationDemo {
     public static void main(String[] args) {
         Table obj = new Table();
-
-        MyThread1 t1 = new MyThread1(obj);
-        MyThread2 t2 = new MyThread2(obj);
-
-        t1.start();
-        t2.start();
+        new MyThread1(obj).start();
+        new MyThread2(obj).start();
     }
 }
